0c8d029bc339528e7ae7b306d0e50c9b6276de18
Konstantin Tornovskiy <coolship@yandex.ru>
2021-03-20 03:59:51 +0300

    Joomla 3.7.0 exploitation Python script repository added

--- tweak/kali.sh
+++ tweak/kali.sh
@@ -10,3 +10,4 @@ git clone https://github.com/saghul/lxd-alpine-builder
 git clone https://github.com/S1ckB0y1337/Active-Directory-Exploitation-Cheat-Sheet#active-directory-exploitation-cheat-sheet
 git clone https://github.com/payloadbox/sql-injection-payload-list
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings
+git clone https://github.com/NinjaJc01/joomblah-3
