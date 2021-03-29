8cc76382d0a239ae4ac0e1dce666d9614db2c884
Konstantin Tornovskiy <coolship@yandex.ru>
2021-03-29 09:36:35 +0300

    Brute tools added New repository added ssh-backdoor description added

--- priv_esc/linux/backdoors/README.md
+++ priv_esc/linux/backdoors/README.md
@@ -1,7 +1,17 @@
 A couple Linux backdoors:
 
     1. Simple SSH backdoor writen on GO
-        
         https://github.com/NinjaJc01/ssh-backdoor
+
+        # HASH generator for custom password
+        https://www.convertstring.com/ru/Hash/SHA512
+        
+        # or in python
+            import hashlib
+            salt = '1c362db832f3f864c8c2fe05f2002a05'
+            password = 'november16'
+            hashlib.sha512(f'{password}{salt}'.encode("utf8")).hexdigest()
+        # out example
+'6d05358f090eea56a238af02e47d44ee5489d234810ef6240280857ec69712a3e5e370b8a41899d0196ade16c0d54327c5654019292cbfe0b5e98ad1fec71bed'
 
     2. 
--- tweak/kali.sh
+++ tweak/kali.sh
@@ -3,6 +3,7 @@
 apt update && apt upgrade -y
 
 cd /opt
+git clone https://github.com/sherlock-project/sherlock
 git clone https://github.com/NinjaJc01/ssh-backdoor
 git clone https://github.com/danielmiessler/SecLists
 git clone https://github.com/zaproxy/zap-extensions
@@ -11,3 +12,7 @@ git clone https://github.com/S1ckB0y1337/Active-Directory-Exploitation-Cheat-She
 git clone https://github.com/payloadbox/sql-injection-payload-list
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings
 git clone https://github.com/NinjaJc01/joomblah-3
+git clone https://github.com/Neohapsis/creddump7
+git clone https://github.com/MuirlandOracle/CVE-2019-15107
+git clone https://github.com/SecureAuthCorp/impacket
+git clone https://github.com/andrew-d/static-binaries
