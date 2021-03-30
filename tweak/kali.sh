b9eb76ee30820535a1ca601d806e32f05f4caa6a
Konstantin Tornovskiy <coolship@yandex.ru>
2021-03-30 22:08:13 +0300

    Tmux config added

--- tweak/kali.sh
+++ tweak/kali.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-apt update && apt upgrade -y
+echo "run this script after system update!"
+echo "$ apt update && apt upgrade -y"
+
+apt install tmux fonts-powerline
+
+mkdir .tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp ./.tmux.conf ~
 
 cd /opt
 git clone https://github.com/sherlock-project/sherlock
@@ -11,6 +18,7 @@ git clone https://github.com/saghul/lxd-alpine-builder
 git clone https://github.com/S1ckB0y1337/Active-Directory-Exploitation-Cheat-Sheet#active-directory-exploitation-cheat-sheet
 git clone https://github.com/payloadbox/sql-injection-payload-list
 git clone https://github.com/swisskyrepo/PayloadsAllTheThings
+git clone https://github.com/pentestmonkey/php-reverse-shell
 git clone https://github.com/NinjaJc01/joomblah-3
 git clone https://github.com/Neohapsis/creddump7
 git clone https://github.com/MuirlandOracle/CVE-2019-15107
